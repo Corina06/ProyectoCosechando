428efe47ad077dbb69b33025c4088c040446361c
Corina06 <corinaruiz1234@gmail.com>
2024-06-27 14:03:37 -0500

    Se agrega el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavComponent } from "./pages/cliente/nav/nav.component";
+import { FooterComponent } from './pages/cliente/footer/footer.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavComponent, RouterLink, FooterComponent]
 })
 export class AppComponent {
   title = 'Cosechando';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+//Rutas Cliente
+import { InicioComponent } from './pages/cliente/inicio/inicio.component';
+import { PedidoComponent } from './pages/cliente/pedido/pedido.component';
+import { CheckoutComponent } from './pages/cliente/checkout/checkout.component';
+import { CarritoComponent } from './pages/cliente/carrito/carrito.component';
+import { ProductoComponent } from './pages/cliente/producto/producto.component';
+import { DetalleproductoComponent } from './pages/cliente/detalleproducto/detalleproducto.component';
+//Rutas Comerciante
+import { PedidosComponent } from './pages/comerciante/pedidos/pedidos.component';
+import { ProductosComponent } from './pages/comerciante/productos/productos.component';
+import { InicioadminComponent } from './pages/comerciante/inicioadmin/inicioadmin.component';
+import { InventarioComponent } from './pages/comerciante/inventario/inventario.component';
+//Rutas Cuenta
+import { LoginComponent } from './pages/cuenta/login/login.component';
+import { RegistroComponent } from './pages/cuenta/registro/registro.component';
+import { NgModule } from '@angular/core';
+
+export const routes: Routes = [
+   //Rutas Cliente
+   {path: '', component: InicioComponent},
+   {path: 'pedido', component: PedidoComponent},
+   {path: 'carrito', component: CarritoComponent},
+   {path: 'checkout', component: CheckoutComponent},
+   {path: 'producto', component: ProductoComponent},
+   {path: 'detalleproducto', component: DetalleproductoComponent},
+   //Rutas Comerciante
+   {path: 'pedidos', component: PedidosComponent},
+   {path: 'productos', component: ProductosComponent},
+   {path: 'inicioadmin', component: InicioadminComponent},
+   {path: 'inventario', component: InventarioComponent},
+   //Rutas Cuenta
+   {path: 'login', component: LoginComponent},
+   {path: 'registro', component: RegistroComponent},
+   {path: '**', redirectTo: '', pathMatch: 'full' },
+
+];
